144e4b63a1af0ffaabfd1dbb4b116e2d106e9547
Ismail Maaji <maajiismail2000@gmail.com>
2025-11-25 15:58:45 +0100

    add

---
+++ week_1/day_1/Exercises_xp_gold/exercises_xp_gold.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+//Exercise — FizzBuzz
+class Program
+{
+        int n = int.Parse(Console.ReadLine());
+
+        if (n % 15 == 0) Console.WriteLine("FizzBuzz");
+        else if (n % 3 == 0) Console.WriteLine("Fizz");
+        else if (n % 5 == 0) Console.WriteLine("Buzz");
+        else Console.WriteLine(n);
+
+}
+
+
+//Exercise : Reverse a Word 
+
+string w = Console.ReadLine();
+for (int i = w.Length - 1; i >= 0; i--)
+    Console.Write(w[i]);
